6a2c8339e89907741b78e1843c31f4f23f4d015c
Eden Yemini <eden881@gmail.com>
2022-07-22 02:07:48 +0300

    Made a mockup version

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 // Imports
 const express = require("express");
-const request = require("request");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const data = require("./data.json");
 
 // Basic configuration
 const port = process.env.PORT || 5000;
 dotenv.config();
 
-const spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
-const spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
-const spotify_redirect_uri = process.env.SPOTIFY_CALLBACK_URI;
-let access_token = "";
-
+// Set up app
 const app = express();
-
-const generateRandomString = function (length) {
-  let randomText = "";
-  const possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (let i = 0; i < length; i++) {
-    randomText += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
-  }
-
-  return randomText;
-};
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 // CORS
 app.use(
@@ -36,53 +26,11 @@ app.use(
 /**
  * Routes
  */
-
-app.get("/auth/login", (_req, res) => {
-  const scope = "streaming user-read-email user-read-private";
-  const state = generateRandomString(16);
-
-  const auth_query_parameters = new URLSearchParams({
-    response_type: "code",
-    client_id: spotify_client_id,
-    scope: scope,
-    redirect_uri: spotify_redirect_uri,
-    state: state,
-  });
-
-  res.redirect(`https://accounts.spotify.com/authorize/?${auth_query_parameters.toString()}`);
+app.get("/api/songs", (_req, res) => {
+  res.send(data.songs);
 });
 
-app.get("/auth/callback", (req, res) => {
-  const code = req.query.code;
-
-  const authOptions = {
-    url: "https://accounts.spotify.com/api/token",
-    form: {
-      code: code,
-      redirect_uri: spotify_redirect_uri,
-      grant_type: "authorization_code",
-    },
-    headers: {
-      Authorization: "Basic " + Buffer.from(spotify_client_id + ":" + spotify_client_secret).toString("base64"),
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    json: true,
-  };
-
-  request.post(authOptions, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      access_token = body.access_token;
-      res.redirect(process.env.FRONTEND_URI);
-    }
-  });
-});
-
-app.get("/auth/token", (_req, res) => {
-  res.json({
-    access_token: access_token,
-  });
-});
-
+// Run app
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
